f6287610eec4898c0e9305df87f82612d2b90806
nushara <nushkavi123@gmail.com>
2024-03-04 20:06:21 +0530

    add projects

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Header />
       <Intro/>
       <Services/>
+      <Projects/>
       <Skills/>
       <Contact/>
       <Footer/>
---
+++ src/components/Projects/ProjectsList.js
@@ -0,0 +1,28 @@
+const ProjectsList = [
+    {
+        projectName: "Auto Paper Marker",
+        projectDesc: "A system to mark an answered structured paper automatically when the image of written answer paper is given with or without the marking scheme."
+    },
+    {
+        projectName: "Beverage Stock Management System",
+        projectDesc: "A system which manages the beverages, sales, customers, and their related operations"       
+    },
+    {
+        projectName: "Attendance marking using sign detection",
+        projectDesc: "A system to get the attendence of the students when the image of the attendence sheet is provided"
+    },
+    {
+        projectName: "Pheonix Airlines",
+        projectDesc: "Airline Management system for managing  the operations like ticket management, flight management and admin operations management"
+    },
+    {
+        projectName: "SalesRep+",
+        projectDesc: "A mobile application designed for the sales representatives to perform the basic operations like product management, shop management and supplier management"
+    },
+    {
+        projectName: "Enjoy Train Travelling",
+        projectDesc: "A website for the train travelers who love to travel around Sri Lanka in trains."
+    }
+]
+
+export default ProjectsList;
